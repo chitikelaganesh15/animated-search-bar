19814fd3f3e6773eebafe1d26ee759d059108b56
Rudra Siva ganesh <chitikelaganesh15@gmail.com>
2026-03-27 11:31:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const searchBox =
+document.querySelector("search-box");
+const button = document.querySelector("search-btn");
 
+button.addEventListener("click", () => {
+    searchBox.classList.toggle("active");
+});
